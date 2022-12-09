eebbc1fe9a0aa0de5e27aa234da39d211b6d6762
Ashton Graves <ashton@ashtongraves.com>
2022-12-08 21:36:59 -0600

    day 3 part 2

--- day3.py
+++ day3.py
@@ -13,7 +13,13 @@ totalPriorities = 0
 for rucksack in rucksacks:
   compartment1 = rucksack[slice(0, len(rucksack)//2)]
   compartment2 = rucksack[slice(len(rucksack)//2, len(rucksack))]
-  commonItem = ''.join(set(compartment1).intersection(set(compartment2)))
+  commonItem = ''.join(set(compartment1) & set(compartment2))
   totalPriorities += getPrioritization(commonItem)
+print(totalPriorities)
 
+totalPriorities = 0
+for i in range(0, len(rucksacks), 3):
+  rucksack1, rucksack2, rucksack3 = rucksacks[i], rucksacks[i+1], rucksacks[i+2]
+  commonItem = ''.join(set(rucksack1) & set(rucksack2) & set(rucksack3))
+  totalPriorities += getPrioritization(commonItem)
 print(totalPriorities)
