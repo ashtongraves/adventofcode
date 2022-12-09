1a8e03c5c84ce44c46c4a82a7804274b6a58f8fd
Ashton Graves <ashton@ashtongraves.com>
2022-12-08 18:44:34 -0600

    completed day1 challenge

---
+++ day1.py
@@ -0,0 +1,13 @@
+inventories = []
+
+with open('day1.txt', 'r') as file:
+  input = file.read()
+  unsplitInventories = input.split('\n\n')
+  for unsplitInventory in unsplitInventories:
+    splitInventory = unsplitInventory.split('\n')
+    formattedInventory = list(map(lambda food: int(food), splitInventory))
+    inventories.append(formattedInventory)
+
+totals = list(map(lambda inventory: sum(inventory), inventories))
+totals.sort(reverse=True)
+print(totals[0])
