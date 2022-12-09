9ebd01366808a7b0fd9f54a058441405386fce07
Ashton Graves <ashton@ashtongraves.com>
2022-12-08 21:04:19 -0600

    day 3 part 1

---
+++ day3.py
@@ -0,0 +1,22 @@
+from webbrowser import get
+
+
+rucksacks = []
+with open('day3.txt', 'r') as file:
+  rucksacks = file.read().splitlines()
+
+def getPrioritization(item):
+  itemUnicode = ord(item)
+  if itemUnicode >= ord('a'):
+    return itemUnicode - ord('a') + 1
+  else:
+    return itemUnicode - ord('A') + 27
+
+totalPriorities = 0
+for rucksack in rucksacks:
+  compartment1 = rucksack[slice(0, len(rucksack)//2)]
+  compartment2 = rucksack[slice(len(rucksack)//2, len(rucksack))]
+  commonItem = ''.join(set(compartment1).intersection(set(compartment2)))
+  totalPriorities += getPrioritization(commonItem)
+
+print(totalPriorities)
